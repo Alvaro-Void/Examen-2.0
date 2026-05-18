9f58fd832c43ed24fad4886a5ccce0167e338c6b
Alvaro-Void <arangela2300@Alumno.ipn.mx>
2026-05-18 03:58:12 -0600

    Update app.py
    errores con render

--- app.py
+++ app.py
@@ -87,8 +87,9 @@ def variable(nombre):
 
 # Ejecutar servidor Flask
 if __name__ == "__main__":
+    import os
 
     app.run(
         host="0.0.0.0",
-        port=5000
+        port=int(os.environ.get("PORT", 5000))
     )
